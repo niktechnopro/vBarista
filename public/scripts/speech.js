2add2cf9ab41a027beb5759bc27c17d04be8a23d
ciscoscholar <ciscoscholar@gmail.com>
2018-03-14 23:04:44 -0400

    websockets added

--- public/scripts/speech.js
+++ public/scripts/speech.js
@@ -1,10 +1,81 @@
-var getSpeechSelector = attr => '[data-speech="' + attr + '"]';
+//we know that Chrome supports websockets(ws use their own protocol)
+const ws = new WebSocket("ws://localhost:8080");
+//next we wire websockets handlers
 
-startBtn = document.querySelector(getSpeechSelector('start'));
+//the following function invoked when websocket opens
+ws.onopen = function(){
+  console.log('websocket opened up')
+  ws.send('connected')
+} 
 
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+//when websocket closes
+ws.onclose = function(){
+  console.log('disconnected')
+}
 
-startBtn.addEventListener('click', () => recognition.start());
-recognition.addEventListener('result', result => console.log(result));
+//speach portion
+var speaker = () => {
 
+  var getSpeechElement = attr => document.querySelector('[data-speech="' + attr + '"]');
+
+  var speechList = getSpeechElement('list');
+  var startBtn = getSpeechElement('start');
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+
+  const Speaker = window.speechSynthesis || window.webkitSpeechSynthesis;
+  // const voices = Speaker.getVoices();
+
+
+  var createSpeechNode = text => {
+    var p = document.createElement('p');
+    p.textContent = text;
+
+    return p;
+  }
+
+  var createUtterance = text => {
+    var utterance = new SpeechSynthesisUtterance(text);
+    return utterance;
+  }
+
+  var sendSpeech = text => {
+      ws.send(text);
+      //when we recieve a message from websocket(they call it even emitting)
+      return new Promise(function(resolve, reject){
+        ws.onmessage = function(payload){
+          console.log('message received', payload.data)
+          resolve(payload.data);
+        }
+    })
+
+    // return $.ajax({
+    //   method: 'post',
+    //   url: url,
+    //   data: {info: text}
+    // })
+  }
+
+  var introduction = createUtterance('What can I get you today?')
+  Speaker.speak(introduction);
+
+  startBtn.addEventListener('click', () => recognition.start());
+  recognition.addEventListener('result', e => {
+    var transcript = e.results[0][0].transcript;
+    var node = createSpeechNode(transcript);
+    speechList.appendChild(node);
+    // console.log(voices);
+    var response = sendSpeech(transcript);
+    // console.log(response)
+    response.then(data => {
+      node = createSpeechNode(data);
+      speechList.appendChild(node);
+      var utterance = createUtterance(data);
+      Speaker.speak(utterance);
+    });
+  });
+
+};
+
+var Speaker = speaker();
--- server/index.js
+++ server/index.js
@@ -4,52 +4,69 @@ const express = require('express');
 const apiai = require('apiai');
 const router = express.Router();
 const app = express();
-const uuidv1 = require('uuid/v1');
 const server = http.createServer(app);
 const port = 3000;
-const uniqueid = uuidv1();//for random number
 const bodyParser = require('body-parser'); //to parse post
 // request through body
+const webSocketServer = require('ws').Server;
+//sockets use their own protocol - ws, instead of http;
+//server portion
 
+wss = new webSocketServer({port: 8080});
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(router);
 
-var array = ['hello', 'how are you', 'nice to meet you']
-var options = {
-    secure: false
-};
+
+// var options = {
+//     secure: false
+// };
+
 const apps = apiai('a27ab2ad6f1b4912a800447c091bba46')//, options);
 
+//I had to plug in dialog flow code inside ws connection to make it work
+//at least for now
+wss.on('connection', function(ws){ //this is a listener for new connection
+	//where (ws) is an individual socket connection
+	var resp;	
+	//the following will send connection back to client
+	ws.send("Welcome to websockets")
 
-app.post('/send',(req,res)=>{
-	let msg = req.body.info;
-	console.log('getting data', req.body)
-
-	var request = apps.textRequest(msg, {
+	ws.on('message', function(message){
+		console.log('received: ', message);
+		var request = apps.textRequest(message, {
 		sessionId: '07eb6b0235874116be00534f1076e04f' //developer
-	});
+		});
 
-	request.on('response', function(response){
-		console.log(response.result.fulfillment.speech)
-		res.json({
-			response
-		})
-	});
+		request.on('response', function(response){
+			// console.log(response.result.fulfillment.speech);
+			resp = response.result.fulfillment.speech;
+		});
+		
+		if (resp){
+			console.log('resp here', resp)
+			ws.send(resp)
+		}
 
-	request.on('error', function(error){
-		console.log(error)
-	});
+		request.on('error', function(error){
+			console.log(error)
+		});
 
-	request.end()
+		request.end()
+	})
 
-});
+	ws.on('close', function(){//if we loose connection
+		console.log("we lost a client")
+	})
+
+	console.log('client connected');//every connection of ws is independent
+})
 
 
 server.listen(port, error => {
-  console.log('Listening!',port);
+  console.log('Listening on port:',port);
   if(error) {
     console.log("There's an error.");
   }
